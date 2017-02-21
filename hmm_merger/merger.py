4a1b77d391861d8eb634cca20180a376816486a4
uym2 <ceciliamaitouyen@gmail.com>
2017-02-20 21:52:16 -0800

    merger

--- hmm_merger/merger.py
+++ hmm_merger/merger.py
@@ -1,6 +1,6 @@
-from sys import argv
+from sys import argv,stdout
 from sequence_lib import read_fasta, write_fasta
-
+from math import log
 
 def ref_matching(aln1,aln2,ref_aln):
 	# for now, assume everything is matched: taxon_name_all == taxon_name1 + taxon_name2 and the order is matched perfectly as well
@@ -43,7 +43,23 @@ def ref_matching(aln1,aln2,ref_aln):
 				print("reference alignment and alignment 2 are not matched at sequence #" + str(j))
 	return matching
 
-def scoring(aln1,aln2,ref_aln):
+def expected_gap(l1,l2,n,m,ref_aln):
+#l1 : len(aln1) ; l2 : len(aln2)
+	eg1 = 0
+	eg2 = 0
+	for i in range(l1):
+		for x in ref_aln[i]:
+			if x == '-':
+				eg1 += 1
+	
+	for i in range(l1,l1+l2):
+		for x in ref_aln[i]:
+			if x == '-':
+				eg2 += 1
+
+	return eg1/n, eg2/m
+
+def estimate_score(aln1,aln2,ref_aln):
 	matching = ref_matching(aln1,aln2,ref_aln)
 	m = len(matching[0])
 	n = len(matching)
@@ -62,9 +78,9 @@ def scoring(aln1,aln2,ref_aln):
 			else:
 				L = L2
 				d = d2
-			if j == len(aln1) and L1 == []:
-				L1 = L1 + [-1]		
-				d1[-1] = len(aln1)
+			#if j == len(aln1) and L1 == []:
+				#L1 = L1 + [-1]		
+				#d1[-1] = len(aln1)
 			if matching[j][i] >= 0:
 				all_gap = False
 				if matching[j][i] in d:
@@ -72,21 +88,133 @@ def scoring(aln1,aln2,ref_aln):
 				else:
 					L += [matching[j][i]]
 					d[matching[j][i]] = 1
-		if L2 == []:
-			L2 = L2 + [-1]
-			d2[-1] = len(aln2)
+		#if L2 == []:
+		#	L2 = L2 + [-1]
+		#	d2[-1] = len(aln2)
 		for x in L1:
 			for y in L2:
-				scoring[(x,y)] = d1[x]*d2[y]
+				if (x,y) not in scoring:
+					scoring[(x,y)] = 0
+				scoring[(x,y)] += d1[x]*d2[y]
 	return scoring
-	
+
+def merge(aln1,aln2,ref_aln):
+	scoring = estimate_score(aln1,aln2,ref_aln)
+	m = len(aln2[0])
+	n = len(aln1[0])
+
+	l1 = len(aln1)
+	l2 = len(aln2)
+
+	eg1,eg2 = expected_gap(l1,l2,n,m,ref_aln)
+	gap_penalty = (l1-eg1)*(l2-eg2)/10
+	print(gap_penalty)
+
+	aln_score = [[0 for i in range(m+1)] for j in range(n+1)]
+	backtrack = [['-' for i in range(m+1)] for j in range(n+1)]
+	for i in range(1,m+1):
+		backtrack[0][i] = 'L'
+		#aln_score[0][i] = aln_score[0][i-1] + scoring[(-1,i-1)] if (-1,i-1) in scoring else aln_score[0][i-1]
+	for j in range(1,n+1):
+		backtrack[j][0] = 'U'
+		#aln_score[j][0] = aln_score[j-1][0] + scoring[(j-1,-1)] if (j-1,-1) in scoring else aln_score[j-1][0]
+
+	for j in range(1,n+1):
+		for i in range(1,m+1):
+			ms  = aln_score[j-1][i-1] + scoring[(j-1,i-1)] if (j-1,i-1) in scoring else aln_score[j-1][i-1]
+			g1 = aln_score[j][i-1] #- gap_penalty
+			g2 = aln_score[j-1][i] #- gap_penalty
+
+			if ms >= g1 and ms >= g2:
+				aln_score[j][i] = ms
+				backtrack[j][i] = 'D'
+			elif g1 >= ms and g1 >= g2:
+				aln_score[j][i] = g1
+				backtrack[j][i] = 'L'
+			else:
+				aln_score[j][i] = g2
+				backtrack[j][i] = 'U'
+
+	i = m
+	j = n
+	M1 = ""
+	M2 = ""
+	while (i > 0 or j > 0):
+		#print(aln_score[j][i])
+		if backtrack[j][i] == 'D':
+			#print('D')
+			#M1 = str(j-1) + M1
+			#M2 = str(i-1) + M2
+			M1 = "." + M1
+			M2 = "." + M2
+			i -= 1
+			j -= 1	
+		elif backtrack[j][i] == 'L':
+			#print('L')
+			#M2 = str(i-1) + M2
+			M2 = "." + M2
+			M1 = "-" + M1
+			i -= 1
+		else:
+			#print('U')
+			M2 = "-" + M2
+			#M1 = str(j-1) + M1
+			M1 = "." + M1
+			j -= 1
+	return aln_score[n][m], M1, M2		
+
+def gap_propagate(cons_seq,targ_seq):
+# propagate gaps from cons_seq to targ_seq; this is used after the cons_seq was aligned with another sequence and we want to
+# propage the alignment to targ_seq. This is a similar idea with transitivity used in PASTA; the ultimate goal is to merge 2 alignments,
+# but in this case we have a consensus sequence for each alignments and we also have a good way (properly using seconday structure) to align them.
+# NOTE: gap_propagate is NOT symmetric: only propagate from consensus to target, not in reverse; careful consider which sequence is the cons_seq and which is targ_seq!
+
+	out_seq = ''
+	i = 0
+	for c in cons_seq:
+		if c != '-':
+			out_seq += targ_seq[i]
+			i += 1
+		else:
+			out_seq += '-'
+
+	return out_seq
 
 aln1_file = argv[1]
 aln2_file = argv[2]
 ref_aln_file = argv[3]
+if len(argv) > 4:
+	outfile = argv[4]
+else:	
+	outfile = None
 
 taxon_name1, aln1 = read_fasta(aln1_file)
 taxon_name2, aln2 = read_fasta(aln2_file)
 taxon_name_all, ref_aln = read_fasta(ref_aln_file)
 
-print(scoring(aln1,aln2,ref_aln))
+for i in range(len(aln1)):
+	aln1[i] = aln1[i].upper()
+for i in range(len(aln2)):
+	aln2[i] = aln2[i].upper()
+for i in range(len(ref_aln)):
+	ref_aln[i] = ref_aln[i].upper()
+
+
+#print(estimate_score(aln1,aln2,ref_aln))
+score,cons1,cons2 = merge(aln1,aln2,ref_aln)
+
+if outfile:
+	fout = open(outfile,'w')
+else:
+	fout = stdout
+
+for i in range(len(aln1)):
+	fout.write(">"+taxon_name1[i]+"\n")
+	fout.write(gap_propagate(cons1,aln1[i])+"\n")
+for i in range(len(aln2)):
+	fout.write(">"+taxon_name2[i]+"\n")
+	fout.write(gap_propagate(cons2,aln2[i])+"\n")
+
+if outfile:
+	fout.close()
+
