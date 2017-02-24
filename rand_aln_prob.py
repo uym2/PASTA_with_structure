790cf4066a48be33e83919f0cf9e492454c40c82
Uyen Mai <uym2@Uyens-MacBook-Pro.local>
2017-02-24 08:17:03 -0800

    New method for rand_aln_prob

--- rand_aln_prob.py
+++ rand_aln_prob.py
@@ -1,55 +1,40 @@
 class rand_Aln:
-	def __init__(self):
-		self.n = 0
-		self.P = {}
-		self.S = {}
-
-	def __init__(self,n):
+	def __init__(self,n=0):
 		self.n = n
-		self.setup(n)
+		self.Q = {}
 
-	def setup(self,n):
-		self.n = n
-		self.P = {}
-		self.S = {}
-
-		for i in range(n+1):
-			self.P[(0,i)] = 1
-			self.S[(0,i)] = 1
+		self.Q[(0,0)] = 1
+		for i in range(1,n+1):
+			self.Q[(0,i)] = self.Q[(0,i-1)]*0.5
 
 		for j in range(1,n+1):
-			self.P[(j,j)] = 2*self.S[(j-1,j-1)] + self.P[(j-1,j)]
-			self.S[(j,j)] = self.S[(j-1,j)] + self.P[(j,j)]
+			self.Q[(j,j)] = 0.25*self.Q[(j-1,j-1)] + self.Q[(j-1,j)]
 			for i in range(j+1,n+1):
-				self.P[(j,i)] = 2*self.S[(j-1,i-1)] + self.P[(j,i-1)]
-				self.S[(j,i)] = self.S[(j-1,i)] + self.P[(j,i)]
+				self.Q[(j,i)] = 0.25*self.Q[(j-1,i-1)] + 0.5*self.Q[(j,i-1)] + 0.5*self.Q[(j-1,i)]
 	
-	def count(self,m1=None,n1=None):
+	def __q(self,m1=None,n1=None):
 		m1 = self.n if m1 is None else m1
 		n1 = self.n if n1 is None else n1
 		
 		if max(m1,n1) > self.n:
-			print("Query number exceeds current size. Please call update(max(" + str(m1) + "," + str(n1) + ")) then try again")
+			print("Query number exceeds current size. Please call extend(max(" + str(m1) + "," + str(n1) + ")) then try again")
 			return None
 	
-		return self.P[(min(m1,n1),max(m1,n1))]	
+		return self.Q[(min(m1,n1),max(m1,n1))]	
 
-	def update(self,n1):
+	def extend(self,n1):
 		if (n1 < self.n):
 			print ("Already up-to-date with the specified size")
 		else:
 			for j in range(self.n+1,n1+1):
-				self.P[(j,j)] = 2*self.S[(j-1,j-1)] + self.P[(j-1,j)]
-				self.S[(j,j)] = self.S[(j-1,j)] + self.P[(j,j)]
+				self.Q[(j,j)] = 0.25*self.Q[(j-1,j-1)] + self.Q[(j-1,j)]
 				for i in range(j+1,n+1):
-					self.P[(j,i)] = 2*self.S[(j-1,i-1)] + self.P[(j,i-1)]
-					self.S[(j,i)] = self.S[(j-1,i)] + self.P[(j,i)]
+					self.Q[(j,i)] = 0.25*self.Q[(j-1,i-1)] + 0.5*Q[(j,i-1)] + 0.5*Q[(j-1,i)]
 			self.n = n1
-			print ("Updated successfully")
+			print ("Matrix Q extended successfully")
 
 	def size(self):
 		return self.n
-
 
 	def prob(self,m1,n1,i,j):
 	# probability of column i of the first seq of length m is matched with column j of the second seq of length n
@@ -57,13 +42,13 @@ class rand_Aln:
 			print("Invalid input")
 			return -1
 		else:
-			return self.count(i-1,j-1)*self.count(m1-i,n1-j)/float(self.count(m1,n1))
+			return 0.25*self.__q(i-1,j-1)*self.__q(m1-i,n1-j)/float(self.__q(m1,n1))
 
 	def in_rate(self,m1,n1,i):
 	# insertion rate (prob of aligning ith column of seq 1 with a gap)
 		p = 0
 		for j in range(n1+1):
-			p += self.count(i-1,j)*self.count(m1-i,n1-j)/float(self.count(m1,n1))
+			p += 0.5*self.__q(i-1,j)*self.__q(m1-i,n1-j)/float(self.__q(m1,n1))
 		return p	
 
 
@@ -71,5 +56,5 @@ class rand_Aln:
 	# deletion rate (prob of aligning jth column of seq 2 with a gap)
 		p = 0
 		for i in range(m1+1):
-			p += self.count(i,j-1)*self.count(m1-i,n1-j)/float(self.count(m1,n1))
+			p += 0.5*self.__q(i,j-1)*self.__q(m1-i,n1-j)/float(self.__q(m1,n1))
 		return p	
